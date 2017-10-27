4a2a1affee9715584149068aa5419c9f0cd35e7a
abolhamd <abolhamd.abotreka@gmail.com>
2017-10-27 11:34:05 +0200

    The code works but needs optimization

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC=gcc
 CFLAGS=-Wall -Wno-unused-value
+LIBFLAG=-lpthread
 
 caltrain: caltrain-runner.c caltrain.c caltrain.h
 	$(CC) $(CFLAGS) -o caltrain caltrain-runner.c -lpthread
--- caltrain.c
+++ caltrain.c
@@ -17,56 +17,55 @@ void station_init(struct station *station) {
 
 void station_load_train(struct station *station, int count) {
 	station->avail_seats = count;
+//	station->on_passen = 0;
 
-	printf("seats from load  %d ,  avail_seats  %d\n", count,
-			station->avail_seats);
+//	printf("seats from load  %d ,  avail_seats  %d\n", count,
+//			station->avail_seats);
 
-	// if there are any free seats , notify all passengers
+// if there are any free seats , notify all passengers
 	if (station->avail_seats > 0)
 		pthread_cond_broadcast(&station->cond_avail_seats);
-
 	pthread_mutex_lock(&station->mutex);
-	while (station->wait_passen > 0 && station->avail_seats > 0) {
 
-//		printf("Here\n");
+
+	while (station->wait_passen > 0 && station->avail_seats > 0)
 		pthread_cond_wait(&station->cond_wait_passen, &station->mutex);
 
-//		printf("HHHEEERRRREEE\n");
-	}
+	station->avail_seats = 0;
 	pthread_mutex_unlock(&station->mutex);
 }
 
 void station_wait_for_train(struct station *station) {
-	// lock the seat mutex
 	pthread_mutex_lock(&station->mutex);
 
-	// increment current waiting passengers
 	station->wait_passen++;
 
-	// wait for available seat and release the mutex
 	while (station->avail_seats == station->on_passen)
 		pthread_cond_wait(&station->cond_avail_seats, &station->mutex);
 
 	station->on_passen++;
 
-	printf("After wait_passen %d\n", station->wait_passen);
+//	printf("After : wait_passen %d , on_passen %d , avail_seats %d\n",
+//			station->wait_passen, station->on_passen, station->avail_seats);
 
-	// release the mutex
 	pthread_mutex_unlock(&station->mutex);
 }
 
 void station_on_board(struct station *station) {
 	pthread_mutex_lock(&station->mutex);
 
+//	printf("On board : wait_passen %d , on_passen %d , avail_seats %d\n",
+//			station->wait_passen, station->on_passen, station->avail_seats);
+
 	station->avail_seats--;
 	station->wait_passen--;
 	station->on_passen--;
 
+	pthread_cond_signal(&station->cond_wait_passen);
 	pthread_mutex_unlock(&station->mutex);
 
 //	printf("Free seats %d , Waiting passengers %d\n", station->avail_seats,
 //			station->wait_passen);
 
 //	if (station->wait_passen == 0 || station->avail_seats == 0)
-	pthread_cond_signal(&station->cond_wait_passen);
 }
